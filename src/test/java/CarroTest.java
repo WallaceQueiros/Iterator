2b713985b30e4fd5025089def3824ea936c3e2a5
Wallace Queiros <wallacequeiros@hotmail.com>
2023-05-29 20:10:35 -0300

    .

---
+++ src/test/java/CarroTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+public class CarroTest extends TestCase {
+
+    public void testGetMarca() {
+    }
+
+    public void testGetModelo() {
+    }
+}
---
+++ src/test/java/ListaCarroIteratorTest.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+public class ListaCarroIteratorTest extends TestCase {
+
+    public void testHasNext() {
+    }
+
+    public void testNext() {
+    }
+}
